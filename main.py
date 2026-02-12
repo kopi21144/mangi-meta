a5a27eb381703a7a3a5ea9eba5897ea71121711a
kopi21144 <kopi21144@gmail.com>
2026-02-12 12:06:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,12 @@ KAPPA_PERIOD = 14
 VEXEL_SMOOTHING = 3
 PHASE_OFFSET_RAD = 0.4128
 CONVERGENCE_FACTOR = 0.0671
+DOMINANT_WEIGHT = 0.62
+SECONDARY_WEIGHT = 0.38
+SEED_NORMALIZER = 1000.0
+CROSSOVER_THRESHOLD = 0.0044
+
+
+@dataclass(frozen=True)
+class OscillatorConfig:
+    """Immutable oscillator parameters."""
